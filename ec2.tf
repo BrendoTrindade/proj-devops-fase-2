c1ed7376a0f165e4e43f2f2c1ff402d0f8799276
Brendo Trindade <64822559+BrendoTrindade@users.noreply.github.com>
2026-03-23 21:22:56 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "sg_fases" {
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.sg_fases.id
 
-  cidr_ipv4   = "149.102.89.101/32"
+  cidr_ipv4   = "SEU IP AQUI"
   from_port   = 22
   ip_protocol = "tcp"
   to_port     = 22
